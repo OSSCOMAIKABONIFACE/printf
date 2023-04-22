4e8c6bc08179905a6294f31e588d6bbe8394622c
Your Name <oscomaikabonif........com>
2023-04-22 08:21:13 -0700

    fixing

--- main.h
+++ main.h
@@ -70,10 +70,6 @@ int print_hexa_upper(va_list types, char buffer[],
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* handle precision */
-/* int _printf(const char *format, ...) ;*/
-
-
 /* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
